da5cbf33e6b81cb28919a1916e44ba6f356c85c9
timolonglaine <104273443+timolonglaine@users.noreply.github.com>
2022-04-23 21:08:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen = pygame.display.set_mode([700,600]
+pygame.display.set_caption("Block + Jump")
+clock = pygame.time.Clock()
+
+while True:
+screen.fill([255,255,255])
+
+player = pygame.draw.rect(screen, (0,0,0), [100,100,40,80]
+
+gedrueckt = pygame.key.get_pressed()
